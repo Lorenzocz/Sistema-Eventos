e9133fe868dcd0cc9f9db425c7e66963ffaf75cf
lcarazo <lcarazo@cfia.cr>
2020-11-19 21:08:18 -0600

    Código

--- lib/app/routes/app_apis.dart
+++ lib/app/routes/app_apis.dart
@@ -1,19 +1,6 @@
 class AppApis {
-   static const ClaveAPi ="AA64FC5B-1169-4A6C-B7E6-EA3034CBB21C";
-  static const ActividadesApi =
-      'https://dev-servicios.cfia.or.cr/CFIAEventosWebApi/api/actividades';
-  static const EventosApi =
-      'https://dev-servicios.cfia.or.cr/CFIAEventosWebApi/api/eventos';
-  static const ConfirmacionApi =
-      'https://dev-servicios.cfia.or.cr/CFIAEventosWebApi/api/confirmacion';
-  static const LoginApi =
-      'https://dev-servicios.cfia.or.cr/CFIAEventosWebApi/api/sso';
-  static const ParticipantesApi =
-      'https://dev-servicios.cfia.or.cr/CFIAEventosWebApi/api/participantes';
-       static const QrApi =
-      'https://dev-servicios.cfia.or.cr/CFIAEventosWebApi/api/qr';
-  static const ConfirmarEntrada =
-      'https://dev-servicios.cfia.or.cr/CFIAEventosWebApi/api/entrada';
-  static const Ingresados =
-      'https://dev-servicios.cfia.or.cr/CFIAEventosWebApi/api/ingresados';
+   /**
+    * Tus Apis
+
+    */
 }
